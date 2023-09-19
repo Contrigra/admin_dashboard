60a75d06e22a0a5c7009e7f5cc03c0fd2a26bdf8
Contrigra <gaponov60b@gmail.com>
2023-09-19 18:00:23 +0300

    feat(announcements): added title, container and cards

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const clamp = require('clamp-js')
+
+const announcementCards = document.querySelectorAll('.announcement-card');
+
+
+
+for (let announcement of announcementCards) {
+    console.log(announcement)
+    clamp(announcement, {clamp: 4, truncationHTML: '<br>'})
+}
